e9beb70aaeb96919a187f0daab840d67bdd9540b
James Watson <watsonjr@gmail.com>
2025-11-05 10:22:25 -0800

    playing with utils

--- CODE/jack_parameters.jl
+++ CODE/jack_parameters.jl
@@ -1,9 +1,9 @@
 
 
 
-λ = 1.0 # risk aversion 
+λ = 1.5 # risk aversion 
 ρ = 0.9 # claims - losses correlation
 cv = 0.0 # variable costs 
-cf = 0.02 # fixed insurance costs
+cf = 0.01 # fixed insurance costs
 ϕ = 0.5 # fraction if income lost in low regime 
 Ū = 1.5 # alternative utility 
